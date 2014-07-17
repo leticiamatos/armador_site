a4bee4450656952c76a9a38c259741e48bd8f5aa
Letícia Matos <leticiahmatos@gmail.com>
2014-07-17 20:19:55 -0300

    home

--- script/utils.js
+++ script/utils.js
@@ -1,3 +1,10 @@
 $(function() {
-
+	$('.slideshow').bxSlider({
+	  nextSelector: '#bt_next_wpr',
+	  prevSelector: '#bt_prev_wpr',
+	  auto: true,
+	  pager: false,
+	  mode: 'fade',
+	  pause: 8000
+	});
 })
